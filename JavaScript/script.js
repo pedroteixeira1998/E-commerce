ffaeed4c66b63dfd6acaa3fd918f3aeeb534a1da
pedroteixeira1998 <pteixeira98@live.com>
2023-02-28 17:54:43 -0300

    Cart

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -46,3 +46,33 @@ const toggleModal = () =>{
     el.addEventListener("click", () => toggleModal());
 });
 
+const addToCartButton = document.querySelector('.button-cart');
+addToCartButton.addEventListener('click', () => {
+
+const productName = document.querySelector('#productName').textContent;
+const productPrice = document.querySelector('#productPrice').textContent;
+const productQuantity = document.querySelector('.input-quantity').value;
+
+const cartItem = document.createElement('div');
+cartItem.classList.add('box');
+cartItem.innerHTML = `
+    <div class="box-image">
+      <img src="imagens/image-product-1.jpg" alt="">
+    </div>
+    <div class="box1">
+      <div class="text">
+        <p>${productName}</p>
+      </div>
+      <div class="box-price"> 
+        <p class="box-price1">${productPrice} x </p>
+        <p class="box-price2">${productQuantity}</p>
+        <p class="box-price3">R$${productPrice * productQuantity},00</p>
+        <span class="material-symbols-outlined" id="lixeira">delete</span>
+      </div>
+    </div>
+  `;
+
+  const cartContainer = document.querySelector('.box');
+  const checkoutButton = document.querySelector('#botão');
+  cartContainer.insertBefore(cartItem, checkoutButton);
+});
